e2f7a72d0e1d12d85985d6458de2f1600a9b37a3
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-12-29 01:24:14 +0100

    added glm

--- src/mainImgui.cpp
+++ src/mainImgui.cpp
@@ -1,94 +1,165 @@
-// #include <iostream>
-// #include <cmath>
-// #include "glad/glad.h"
-// #include "GLFW/glfw3.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <vector>
+#include "global.hpp"
+#include "Shader.h"
+#include "CPU/BoidsRenderer.h"
+#include "CPU/Boid.h"
+#include "CPU/Flock.h"
 
-// #include "imgui.h"
-// #include "imgui_impl_glfw.h"
-// #include "imgui_impl_opengl3.h"
-// #include "imgui_internal.h"
+#include "GPU/FlockGPU.h"
+#include "GPU/FlockGridGPU.h"
+#include "GPU/cuda_functions.h"
+#include <chrono>
+#include <thread>
 
-// #include "UI.h"
+#include "nlohman/json.hpp"
 
-// // int mainImgui()
-// int main()
-// {
-//     if (!glfwInit())
-//         return -1;
+#include "UI.h"
 
-// #if defined(IMGUI_IMPL_OPENGL_ES2)
-//     // GL ES 2.0 + GLSL 100
-//     const char *glsl_version = "#version 100";
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-//     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-// #elif defined(__APPLE__)
-//     // GL 3.2 + GLSL 150
-//     const char *glsl_version = "#version 150";
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
-//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
-// #else
-//     // GL 3.0 + GLSL 130
-//     const char *glsl_version = "#version 130";
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-//     // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-//     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-// #endif
+using namespace std;
+using json = nlohmann::json;
 
-//     GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-//     if (!window)
-//     {
-//         glfwTerminate();
-//         return -1;
-//     }
-//     glfwMakeContextCurrent(window);
-//     glfwSwapInterval(1);
+void displayFPS();
 
-//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//     {
-//         std::cout << "Couldn't load opengl" << std::endl;
-//         glfwTerminate();
-//         return -1;
-//     }
+void loadParamsFromFile(Params& params) {
+    fstream file("../src/parameters.json");
+    if (!file.is_open()) 
+    {
+        cerr << "Error opening file" << endl;
+        exit(1);
+    }
 
-//     IMGUI_CHECKVERSION();
+    nlohmann::json j;
+    file >> j;
+
+    params.width = j["width"].get<int>();
+    params.height = j["height"].get<int>();
+    params.numberOfSegments = j["numberOfSegments"].get<int>();
+    params.radius = j["radius"].get<float>();
+    params.start_x = j["start_x"].get<float>();
+    params.start_y = j["start_y"].get<float>();
+    params.perception = j["perception"].get<float>();
+    params.borderForce = j["borderForce"].get<float>();
+    params.minSpeed = j["minSpeed"].get<float>();
+    params.maxSpeed = j["maxSpeed"].get<float>();
+    params.maxForce = j["maxForce"].get<float>();
+    params.alignmentForce = j["alignmentForce"].get<float>();
+    params.cohesionForce = j["cohesionForce"].get<float>();
+    params.separationForce = j["separationForce"].get<float>();
+    
+    params.cellSize = params.perception;
+    params.widthCount = (params.width + params.cellSize - 1) / params.cellSize;
+    params.heightCount = (params.height + params.cellSize - 1) / params.cellSize;
+    params.cellCount = params.widthCount * params.heightCount;
+}
+
+int main()
+{
+    GLFWwindow *window;
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    window = glfwCreateWindow(screenWidth, screenHeight, "Window!", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Cound't load opengl" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+//    IMGUI_CHECKVERSION();
 //     ImGui::CreateContext();
-//     ImGuiIO &io = ImGui::GetIO();
-//     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-//     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+//     ImGuiIO& io = ImGui::GetIO(); (void)io;
+//     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+//     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+//     ImGui::StyleColorsDark();
+//     //ImGui::StyleColorsLight();
 
+//     // Setup Platform/Renderer backends
 //     ImGui_ImplGlfw_InitForOpenGL(window, true);
+//     const char* glsl_version = "#version 150";
 //     ImGui_ImplOpenGL3_Init(glsl_version);
 
-//     glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+//     bool show_demo_window = true;
+//     bool show_another_window = false;
+//     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    //   if (show_demo_window)
+    //         ImGui::ShowDemoWindow(&show_demo_window);
 
-//     bool simulation = true;
-//     while (!glfwWindowShouldClose(window))
-//     {
-//         glfwWaitEvents();
+    //  ImGui::Begin("Hello, world!");    
+            //  ImGui::Text("Hello from another window!");
 
-//         ImGui_ImplOpenGL3_NewFrame();
-//         ImGui_ImplGlfw_NewFrame();
-//         ImGui::NewFrame();
+    Shader shader("../src/10.1.instancing.vs", "../src/10.1.instancing.fs");
+    shader.use();
+    shader.setVec3("u_Color", glm::vec3(0.0f, 1.0f, 1.0f));
 
-//         simulation = ToggleSwitch("Toggle", simulation);
+    glm::mat4 projection = glm::ortho(0.0f, screenWidth, 0.0f, screenHeight);
+    glm::mat4 model = glm::mat4(1.0f);
 
-//         glClear(GL_COLOR_BUFFER_BIT);
+    glm::mat4 mvp = projection * model;
 
-//         ImGui::Render();
-//         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    shader.setMat4("u_mvp", mvp);
 
-//         glfwSwapBuffers(window);
-//     }
 
-//     ImGui_ImplOpenGL3_Shutdown();
-//     ImGui_ImplGlfw_Shutdown();
-//     ImGui::DestroyContext();
+    int boidsCount = 500;
 
-//     glfwTerminate();
+    Params params;
 
-//     return 0;
-// }
+    loadParamsFromFile(params);
+
+    FlockGridGPU flockGridGPU(boidsCount, params);
+    BoidsRenderer boidsRenderer(boidsCount, flockGridGPU.translations, flockGridGPU.colorIndex);    
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        loadParamsFromFile(params);
+        glfwPollEvents();
+        
+        double cursorX, cursorY;
+        glfwGetCursorPos(window, &cursorX, &cursorY);
+        params.cursorX = cursorX;
+        params.cursorY = params.height - cursorY;
+
+        flockGridGPU.computeNextFrame(&(boidsRenderer.cuda_vbo_resource), params);
+
+        boidsRenderer.clear();
+
+        boidsRenderer.draw();
+
+        glfwSwapBuffers(window);
+
+        displayFPS();
+    }
+    glfwTerminate();
+    return 0;
+}
+
+auto lastTime = chrono::high_resolution_clock::now();
+int frameCount = 0;
+
+void displayFPS()
+{
+    auto currentTime = chrono::high_resolution_clock::now();
+    frameCount++;
+
+    if (chrono::duration_cast<chrono::seconds>(currentTime - lastTime).count() >= 2)
+    {
+        double fps = frameCount / 2.0;
+        cout << "FPS: " << fps << endl;
+
+        lastTime = currentTime;
+        frameCount = 0;
+    }
+}
